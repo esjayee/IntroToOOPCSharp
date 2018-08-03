b46211cc44ea04471a043e5a328b2640d9489ac7
susan.emmons <susan.emmons@AG007075>
2018-08-03 17:27:28 +0100

    Mod 2 Lab 1

---
+++ Mod2_Lab1/Mod2_Lab1/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Mod2_Lab1
+{
+    class Employee
+    {
+        // 3 private member variables: Name, Salary and id
+        private string empName;
+        private double empBaseSalary;
+        private int empId;
+
+        public string Name { get => empName; set => empName = value; }
+        public double BaseSalary { get => empBaseSalary; set => empBaseSalary = value; }
+        public int ID { get => empId; set => empId = value; }
+
+        // creates integer variable called empCount and assigns value to 1
+        private static int empCount = 1;
+
+        // constructor
+        public Employee(string name, double baseSalary)
+        {
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+
+            this.ID = empCount++;
+        }
+
+        // this method returns the employee's base salary
+        public double getBaseSalary()
+        {
+            return this.BaseSalary;
+        }
+
+        // this method returns the employee's name
+        public string getName()
+        {
+            return this.Name;
+        }
+
+        // this method returns the employee's ID
+        public int getEmployeeID()
+        {
+            return this.ID;
+        }
+
+        // this method returns the employee's ID and name
+        public string toString()
+        {
+            return this.Name + " (" + this.ID + ")";
+        }
+
+        // this method returns the employee's ID and Name and confirms that the employee is in the system
+        public virtual string employeeStatus()
+        {
+            return this.toString() + " is in the company's system";
+        }
+    }
+}
---
+++ Mod2_Lab1/Mod2_Lab1/TechnicalEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod2_Lab1
+{
+    // TechnicalEmployee Class inheriting from Employee Class
+    class TechnicalEmployee : Employee
+    {
+        // creates integer variable called successfulCheckIns and assigns value to 5
+        public int successfulCheckIns = 5;
+
+        // calls upon base-class (EmployeeClass) constructor from within derived class (TechnicalEmployee Class)
+        public TechnicalEmployee(string name) : base(name, 75000)
+        {
+
+        }
+
+        public override string employeeStatus()
+        {
+            return this.toString() + " has " + this.successfulCheckIns + "successfull check ins";
+        }
+    }
+}
