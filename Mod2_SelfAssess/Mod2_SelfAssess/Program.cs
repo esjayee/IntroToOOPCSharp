dfd2f43cfecbc5c887b662174f31dfbb433f9b77
esjayee <semmons@mac.com>
2018-08-07 00:11:37 +0100

    Updates

--- Mod2_SelfAssess/Mod2_SelfAssess/Program.cs
+++ Mod2_SelfAssess/Mod2_SelfAssess/Program.cs
@@ -2,20 +2,16 @@
 
 namespace Mod2_SelfAssess
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var aTeacher = new Teacher
-            {
-                Forename = "Ena",
-                Surname = "McQuinn"
-            };
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var aTeacher = new Teacher("Ena", "McQuinn");
 
-            //   aTeacher.Dob = DateTime.Parse("1967-01-13");
-            // Console.WriteLine($"DoB: {aTeacher.Dob:dd-MMMM-yyyy}");
+			aTeacher.Dob = DateTime.Parse("1967-01-13");
+			Console.WriteLine($"DoB: {aTeacher.Dob:dd-MMMM-yyyy}");
 
-            Console.WriteLine(aTeacher.ToString());
-        }
-    }
+			Console.WriteLine(aTeacher.ToString());
+		}
+	}
 }
--- Mod2_SelfAssess/Mod2_SelfAssess/Teacher.cs
+++ Mod2_SelfAssess/Mod2_SelfAssess/Teacher.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace Mod2_SelfAssess
 {
-    class Teacher : Person
-    {
-    }
+	class Teacher : Person
+	{
+		public Teacher(string firstname, string surname) : base(firstname, surname)
+		{
+		}
+	}
 }
